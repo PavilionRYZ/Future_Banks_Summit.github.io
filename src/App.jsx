c8bf83b0781ec71327871c230333681140478c73
Subhra sundar sinha <subhrasundarsinha21@gmail.com>
2025-12-12 12:58:40 +0530

    update reg

--- src/App.jsx
+++ src/App.jsx
@@ -21,7 +21,7 @@ function App() {
       <SpeakersSection />
       <AgendaSection />
       <SponsorsSection />
-      <RegistrationForm />
+      {/* <RegistrationForm /> */}
       <ContactSection />
       <Footer />
     </div>
